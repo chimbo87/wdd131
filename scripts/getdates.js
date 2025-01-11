986a0fbc71034920e34006d84430a6a48dc95646
Archford Nhanga <archfordnhanga@gmail.com>
2025-01-11 21:07:55 +0200

    designs

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Get the current year for the copyright
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+
+document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
